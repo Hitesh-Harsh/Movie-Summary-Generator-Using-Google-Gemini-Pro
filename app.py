3ebfbbce86f010a7c7a038808b6315cb99134d32
Hitesh Harsh <95738820+Hitesh-Harsh@users.noreply.github.com>
2024-05-13 18:06:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,11 @@
 import streamlit as st
 import google.generativeai as genai
-from dotenv import load_dotenv
-import os  # Import os module for accessing environment variables
+from youtube_transcript_api import YouTubeTranscriptApi
+import os
 
-# Load environment variables
-load_dotenv()
-
-# Configure GenerativeAI with API Key from environment variables
+# Configure GenerativeAI with the API key from environment variable
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
-# Prompt for the GenerativeAI model
 prompt = "You are a Youtube Video Summarizer. You will be taking the transcript text and summarizing the entire video and providing the important summary points in 250 Words in English. The Transcript text will be appended here"
 
 def extract_transcript_details(youtube_video_url):
@@ -33,7 +29,6 @@ youtube_link = st.text_input("Enter YouTube Video Link:")
 
 if youtube_link:
     video_id = youtube_link.split("=")[1]
-    print(video_id)
     st.image(f"http://img.youtube.com/vi/{video_id}/0.jpg", use_column_width=True)
 
 if st.button("Get Detailed Notes"):
